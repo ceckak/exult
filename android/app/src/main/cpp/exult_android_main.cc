385ace756794078e9cb60736f172575a9357a748
Ken Cecka <ceckak@uw.edu>
2021-07-25 22:14:33 -0700

    Change ExultActivity to an SDLActivity and launch Exult
    This patch transitions the app to a propper SDL android activity and launches Exult.  With this patch, the game is capable of launching, but requires manually staging the game data files and lacks android touchui support.

--- android/app/src/main/cpp/exult_android_main.cc
+++ android/app/src/main/cpp/exult_android_main.cc
@@ -1,5 +1,6 @@
 #include "android_log_streambuf.h"
 
+#include <SDL2/SDL_main.h>
 #include <android/log.h>
 #include <jni.h>
 
@@ -10,6 +11,8 @@ extern "C" JNIEXPORT int JNICALL ExultAndroid_main(int argc, char* argv[]) {
 	AndroidLog_streambuf exult_cerr(ANDROID_LOG_ERROR, "exult-android-wrapper");
 	auto                 ndk_cout = std::cout.rdbuf(&exult_cout);
 	auto                 ndk_cerr = std::cerr.rdbuf(&exult_cerr);
+
+        auto result = SDL_main(argc, argv);
 
 	std::cout.rdbuf(ndk_cout);
 	std::cerr.rdbuf(ndk_cerr);
--- android/app/src/main/java/info/exult/ExultActivity.java
+++ android/app/src/main/java/info/exult/ExultActivity.java
@@ -1,13 +1,31 @@
 package info.exult;
 
-import android.app.Activity;
-import android.os.Bundle;
+import org.libsdl.app.SDLActivity;
 
-public class ExultActivity extends Activity
+public class ExultActivity extends SDLActivity
 {
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_main);
-   }
+
+    /**
+     * This method returns the name of the application entry point
+     * It can be overridden by derived classes.
+     */
+    protected String getMainFunction() {
+        return "ExultAndroid_main";
+    }
+
+    /**
+     * This method is called by SDL before loading the native shared libraries.
+     * It can be overridden to provide names of shared libraries to be loaded.
+     * The default implementation returns the defaults. It never returns null.
+     * An array returned by a new implementation must at least contain "SDL2".
+     * Also keep in mind that the order the libraries are loaded may matter.
+     *
+     * @return names of shared libraries to be loaded (e.g. "SDL2", "main").
+     */
+    protected String[] getLibraries() {
+        return new String[] {
+            "SDL2",
+            "exult-android-wrapper"
+        };
+    }
 }
