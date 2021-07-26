8b09c35e10a0b6cbf0398dcea76d787b1907fb41
Ken Cecka <ceckak@uw.edu>
2021-07-25 22:14:33 -0700

    Add a skeleton Android app
    This is just a simple hello-world app that will eventually turn into the android Java layer that launches exult.  This first draft is a placeholder to enable testing the autoconf changes to build an android APK.

---
+++ android/app/src/main/java/info/exult/ExultActivity.java
@@ -0,0 +1,13 @@
+package info.exult;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExultActivity extends Activity
+{
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+   }
+}
