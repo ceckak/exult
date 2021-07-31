57c12171ca98eae10bbda35352f881295b2b09cf
Ken Cecka <ceckak@uw.edu>
2021-07-30 21:27:35 -0700

    Add Xar support for apache commons compress
    A subsequent patch will add support for importing game content from arbitrary archives by leveraging apache commons compress.  However, the only easily extractable Ultima archives from Good Old Games come in the form of a Xar archive, which is not supported by apache commons compress.  This patch adds support for extracting Xar archives with apache commons compress.

---
+++ android/app/src/main/java/info/exult/XarArchiveEntry.java
@@ -0,0 +1,35 @@
+package info.exult;
+
+import java.util.Date;
+
+import org.apache.commons.compress.archivers.ArchiveEntry;
+
+import com.sprylab.xar.XarEntry;
+
+class XarArchiveEntry implements ArchiveEntry {
+    private final XarEntry m_xarEntry;
+
+    XarArchiveEntry(XarEntry xarEntry) {
+        m_xarEntry = xarEntry;
+    }
+
+    @Override
+    public String getName() {
+        return m_xarEntry.getName();
+    }
+
+    @Override
+    public long getSize() {
+        return m_xarEntry.getSize();
+    }
+
+    @Override
+    public boolean isDirectory() {
+        return m_xarEntry.isDirectory();
+    }
+
+    @Override
+    public Date getLastModifiedDate() {
+        return m_xarEntry.getTime();
+    }
+}
