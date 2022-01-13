59fa1df7e4d618bd010332d7a86e4f46473c36c8
ceckak <ceckak@uw.edu>
2022-01-12 21:56:23 -0800

    Add autolaunch checkbox
    This adds a checkbox to the launcher which is off by default, but can be turned on so that starting the app will bypass the launcher UI and go directly into Exult.  Users can always exit the Exult game engine to get back to the launcher UI if they want to make changes or disable autolaunch.

--- android/app/src/main/java/info/exult/LauncherFragment.java
+++ android/app/src/main/java/info/exult/LauncherFragment.java
@@ -8,20 +8,43 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.SharedPreferences;
 import android.content.Intent;
 
 import android.util.Log;
 
 public class LauncherFragment extends Fragment implements View.OnClickListener {
+  private static final String AUTO_LAUNCH_KEY = "autoLaunch";
+  private SharedPreferences m_sharedPreferences;
+
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
 
       View view = inflater.inflate(R.layout.launcher_card, container, false);
 
+      // Route auto-launch toggles to this class
+      CheckBox autoLaunchCheckBox = (CheckBox) view.findViewById(R.id.autoLaunchCheckBox);
+      autoLaunchCheckBox.setOnClickListener(this);
+
+      // Set the initial auto-launch state
+      Context context = view.getContext();
+      ContentResolver contentResolver = context.getContentResolver();
+      m_sharedPreferences = context.getSharedPreferences("launcherFragment", Context.MODE_PRIVATE);
+      boolean autoLaunch = m_sharedPreferences.getBoolean(AUTO_LAUNCH_KEY, false);
+      autoLaunchCheckBox.setChecked(autoLaunch);
+
       // Route launch clicks to this class
       Button launchExultButton = (Button) view.findViewById(R.id.launchExultButton);
       launchExultButton.setOnClickListener(this);
+
+      // Start the game if auto-launch is enabled
+      if (autoLaunch) {
+          launchExult();
+      }
 
       return view;
   }
@@ -29,6 +52,12 @@ public class LauncherFragment extends Fragment implements View.OnClickListener {
   @Override
   public void onClick(View view) {
       switch (view.getId()) {
+      case R.id.autoLaunchCheckBox:
+          CheckBox checkBox  = (CheckBox) view;
+          boolean checked = checkBox.isChecked();
+          SharedPreferences.Editor sharedPreferencesEditor = m_sharedPreferences.edit();
+          sharedPreferencesEditor.putBoolean(AUTO_LAUNCH_KEY, checked).commit();
+          break;
       case R.id.launchExultButton:
           launchExult();
           break;
