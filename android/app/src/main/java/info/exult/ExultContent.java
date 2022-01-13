3606c56f0e045206a5a2334024f53ac465db2801
Ken Cecka <ceckak@uw.edu>
2022-01-12 21:56:22 -0800

    Add classes for reading Exult content from archives
    Setting up Exult on a desktop typically involves manually installing original game files, optional audio packs, and optional mods, and editing exult.cfg to point to their locations.  This gets particularly awkward for mobile devices.  To ease the process, the Android version of Exult includes a launcher which allows the user to browse to downloaded content archives and it rummages around inside them to find the correct content to install into the correct game folder.  This patch adds the classes that implement the archive extraction, validation, and installation.  A subsequent patch will implement the UI which drives the process.
    
    Note that this is currently implemented in Java and only applicable to Android, but could (and probably should) be re-implemented in the C++ layer so that other platforms could make use of it as well.

---
+++ android/app/src/main/java/info/exult/ExultContent.java
@@ -0,0 +1,249 @@
+package info.exult;
+
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Set;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.util.ArraySet;
+import android.util.Log;
+
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.compressors.CompressorException;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
+
+/// Base class for managing Exult content in the Android app
+public abstract class ExultContent {
+    private final String m_type;
+    private final String m_name;
+    private final ContentResolver m_contentResolver;
+    private final SharedPreferences m_sharedPreferences;
+    private static final String MANIFEST_KEY = "manifest";
+    private Set<String> m_manifest;
+
+    protected ExultContent(String type, String name, Context context) {
+        m_type = type;
+        m_name = name;
+        m_contentResolver = context.getContentResolver();
+        m_sharedPreferences = context.getSharedPreferences("content." + type + "." + name, Context.MODE_PRIVATE);
+        m_manifest = m_sharedPreferences.getStringSet(MANIFEST_KEY, null);
+    }
+
+    public String getType() {
+        return m_type;
+    }
+
+    public String getName() {
+        return m_name;
+    }
+
+    public boolean isInstalled() {
+        return null != m_manifest;
+    }
+
+    public boolean install(Uri sourceUri) throws ArchiveException, FileNotFoundException, IOException {
+        if (isInstalled()) {
+            Log.d("ExultContent", "already installed");
+            return false;
+        }
+        m_manifest = new ArraySet<String>();
+        ArchiveEntryVisitor identifyVisitor =
+            (location, archiveEntry, inputStream) -> { return identify(location, archiveEntry, inputStream); };
+        ArchiveEntryVisitor installVisitor =
+            (location, archiveEntry, inputStream) -> { return install(location, archiveEntry, inputStream); };
+        return traverse(sourceUri, identifyVisitor) && traverse(sourceUri, installVisitor);
+    }
+
+    public boolean uninstall() throws IOException {
+        if (!isInstalled()) {
+            // Already not installed.
+            return true;
+        }
+
+        // First try to delete all the files.  For simplicity, not bothering to clean up empty directories.
+        // If there are problems deleting any individual files, we'll still proceed with the uninstall, but return
+        // false to indicate that it was not a clean uninstall.
+        boolean cleanUninstall = true;
+        for (String pathString : m_manifest) {
+            Path path = Paths.get(pathString);
+            if (Files.exists(path) && !Files.isDirectory(path)) {
+                if (!Files.deleteIfExists(path)) {
+                    Log.d("ExultContent", "unable to delete " + path);
+                    cleanUninstall = false;
+                }
+            }
+        }
+
+        // Clear the manifest.
+        SharedPreferences.Editor sharedPreferencesEditor = m_sharedPreferences.edit();
+        sharedPreferencesEditor.remove(MANIFEST_KEY).commit();
+        m_manifest = null;
+
+        return cleanUninstall;
+    }
+
+    protected abstract boolean identify(Path location, ArchiveEntry archiveEntry, InputStream inputStream) throws IOException;
+    protected abstract Path getContentRootInArchive();
+    protected abstract Path getContentInstallRoot();
+
+    protected Path getInstallDestination(Path location, ArchiveEntry archiveEntry) {
+        // Skip over directories.
+        if (archiveEntry.isDirectory()) {
+            return null;
+        }
+
+        // If we haven't identified a data root, then we can't determine the installation destination.
+        Path contentRootInArchive = getContentRootInArchive();
+        if (null == contentRootInArchive) {
+            return null;
+        }
+
+        // Skip over archive entries that fall outside the game root.
+        Path fullPath = getFullArchivePath(location, archiveEntry);
+        Path parent = fullPath.getParent();
+        boolean nullParent = null == parent;
+        boolean emptyRoot = contentRootInArchive.toString().isEmpty();
+        if (!emptyRoot && (nullParent || !fullPath.startsWith(contentRootInArchive))) {
+            return null;
+        }
+
+        // Strip off the root.
+        Path relativePath = contentRootInArchive.relativize(fullPath);
+
+        // Map into local install dir
+        return getContentInstallRoot().resolve(relativePath);
+    }
+
+    private boolean install(Path location, ArchiveEntry entry, InputStream inputStream) throws IOException {
+        // Null entry indicates this is the terminal call after completing the DFS traversal of the content.
+        if (null == entry) {
+            // For an install pass, the terminal call is where we record the manifest and return true to indicate a
+            // successful installation.
+            SharedPreferences.Editor sharedPreferencesEditor = m_sharedPreferences.edit();
+            sharedPreferencesEditor.putStringSet(MANIFEST_KEY, m_manifest).commit();
+            return true;
+        }
+
+        // Don't try to install directory entries; we'll create them when we have content to populate them with.
+        if (entry.isDirectory()) {
+            return false;
+        }
+
+        // If this entry doesn't have a destination, move on.
+        Path destination = getInstallDestination(location, entry);
+        if (null == destination) {
+            return false;
+        }
+
+        // Create any missing directories.
+        Path parent = destination.getParent();
+        if (null != parent) {
+            Files.createDirectories(parent);
+        }
+
+        // Install the file.
+        Files.copy(inputStream, destination, StandardCopyOption.REPLACE_EXISTING);
+
+        //Record this file in the manifest for the content.
+        m_manifest.add(destination.toString());
+
+        return false;
+    }
+
+    private interface ArchiveEntryVisitor {
+        public boolean accept(Path location, ArchiveEntry archiveEntry, InputStream inputStream) throws IOException;
+    }
+
+    private boolean traverse(Uri sourceUri, ArchiveEntryVisitor visitor) throws ArchiveException, FileNotFoundException, IOException {
+        class LocationAndArchive {
+            LocationAndArchive(Path location, ArchiveInputStream archive) {
+                this.location = location;
+                this.archive = archive;
+            }
+            Path location;
+            ArchiveInputStream archive;
+        };
+        Deque<LocationAndArchive> archiveStack = new ArrayDeque<LocationAndArchive>();
+        InputStream inputStream = m_contentResolver.openInputStream(sourceUri);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+        ArchiveStreamFactory archiveStreamFactory = new ArchiveStreamFactoryWithXar();
+        CompressorStreamFactory compressorStreamFactory = new CompressorStreamFactory();
+
+
+        // Set up the initial top-level archive in the stack to start processing.
+        archiveStack.push(new LocationAndArchive(null, archiveStreamFactory.createArchiveInputStream(bufferedInputStream)));
+
+        // Proceed with DFS archive processing until the visitor terminates the search.
+        while (!archiveStack.isEmpty()) {
+            LocationAndArchive locationAndArchive = archiveStack.pop();
+            ArchiveEntry archiveEntry = null;
+
+            // Process all the entries in the current archive.
+            while (null != (archiveEntry = locationAndArchive.archive.getNextEntry())) {
+
+                // Visit this entry and check if we can terminate the traversal.
+                if (visitor.accept(locationAndArchive.location, archiveEntry, locationAndArchive.archive)) {
+                    return true;
+                }
+
+                // If this is a directory, there's nothing more to do here; the subsequent archive entries will
+                // descend into the subdirectories.
+                if (archiveEntry.isDirectory()) {
+                    continue;
+                }
+
+                // This is a non-directory entry; see if it is something we can decompress.
+                InputStream nestedInputStream = new BufferedInputStream(locationAndArchive.archive);
+                try {
+                    nestedInputStream = new BufferedInputStream(compressorStreamFactory.createCompressorInputStream(nestedInputStream));
+                } catch (CompressorException e) {
+                    // Ignore exception; this was a speculative attempt to decompress.
+                }
+
+                // Irrespective of whether we are decompressing, see if this is a nested archive we can descend into.
+                ArchiveInputStream nestedArchiveInputStream = null;
+                try {
+                    nestedArchiveInputStream = archiveStreamFactory.createArchiveInputStream(nestedInputStream);
+                } catch (ArchiveException e) {
+                    // Ignore exception; this was a speculative attempt to extract an archive.
+                }
+
+                // If it's not any sort of archive, there's nothing to descend into an we can move on.
+                if (null == nestedArchiveInputStream) {
+                    continue;
+                }
+
+                // This looks like a nested archive; descend into it.
+                archiveStack.push(locationAndArchive);
+                Path nestedArchivePath = getFullArchivePath(locationAndArchive.location, archiveEntry);
+                locationAndArchive = new LocationAndArchive(nestedArchivePath, nestedArchiveInputStream);
+            }
+        }
+
+        // Make a terminating call to the visitor in case it has any final steps or mitigations to attempt.
+        return visitor.accept(null, null, null);
+    }
+
+    protected static Path getFullArchivePath(Path location, ArchiveEntry archiveEntry) {
+        // Full path includes concatenating location and the archive entry.  Note that the archiveEntry name may
+        // include subdirectories.
+        if (null == location) {
+            return Paths.get(archiveEntry.getName());
+        }
+        return location.resolve(archiveEntry.getName());
+    }
+}
