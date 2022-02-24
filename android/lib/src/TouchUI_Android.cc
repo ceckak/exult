d7fd555cdc5b4b6cd8ab1f37b9bac24779e429a5
Ken Cecka <ceckak@uw.edu>
2022-02-23 20:45:17 -0800

    Add TouchUI_Android skeleton
    iOS support uses a TouchUI class to manage several UI elements.  This patch adds a skeleton implementation for Android.  This is also the first Android-specific class added to the build, so this patch also introduces an android library that gets linked into Exult.  Any future android-specific code could be bundled into this library.

---
+++ android/lib/src/TouchUI_Android.cc
@@ -0,0 +1,23 @@
+#include <TouchUI_Android.h>
+
+
+TouchUI_Android::TouchUI_Android() {
+}
+
+void TouchUI_Android::promptForName(const char *name) {
+}
+
+void TouchUI_Android::showGameControls() {
+}
+
+void TouchUI_Android::hideGameControls() {
+}
+
+void TouchUI_Android::showButtonControls() {
+}
+
+void TouchUI_Android::hideButtonControls() {
+}
+
+void TouchUI_Android::onDpadLocationChanged() {
+}
